3577946069e6001e4665da952938fc3b5eee19c5
Nikhill Rao <rnikhill@umich.edu>
2021-12-12 18:27:02 -0500

    change board type

--- src/Grid.hs
+++ src/Grid.hs
@@ -8,44 +8,51 @@ import Data.Maybe
 import qualified Data.Set as Set
 import Text.Read
 
-data Board = Board Int (Array (Int, Int) (Maybe Int))
+data Square = Fixed Int | Possible (Set.Set Int)
+data Board = Board Int (Array (Int, Int) Square)
 
-mkBoard :: Int -> Board
-mkBoard rank = Board rank arr
-  where
-    arr = array ((1, 1), (rank ^ 2, rank ^ 2)) [((i, j), Nothing) | i <- [1 .. rank ^ 2], j <- [1 .. rank ^ 2]]
+isEmpty :: Square -> Bool
+isEmpty (Possible _) = True
+isEmpty _ = False
 
-getRow :: Board -> Int -> [Maybe Int]
+toString :: Square -> String
+toString (Possible _) = "*"
+toString (Fixed n) = show n
+
+getNumbers :: [Square] -> Set.Set Int
+getNumbers squares = Set.fromList [x | Fixed x <- squares]
+
+getRow :: Board -> Int -> [Square]
 getRow (Board rank arr) i = [arr ! (i, j) | j <- [1 .. rank ^ 2]]
 
-getCol :: Board -> Int -> [Maybe Int]
+getCol :: Board -> Int -> [Square]
 getCol (Board rank arr) j = [arr ! (i, j) | i <- [1 .. rank ^ 2]]
 
-getSector :: Board -> (Int, Int) -> [Maybe Int]
+getSector :: Board -> (Int, Int) -> [Square]
 getSector (Board rank arr) (i, j) =
   let is = [(i - 1) * rank + 1 .. i * rank]
       js = [(j - 1) * rank + 1 .. j * rank]
    in [arr ! (i, j) | i <- is, j <- js]
 
-getRowNeighbors :: Board -> (Int, Int) -> [Maybe Int]
+getRowNeighbors :: Board -> (Int, Int) -> [Square]
 getRowNeighbors board (i, _) = getRow board i
 
-getColNeighbors :: Board -> (Int, Int) -> [Maybe Int]
+getColNeighbors :: Board -> (Int, Int) -> [Square]
 getColNeighbors board (_, j) = getCol board j
 
-getSectorNeighbors :: Board -> (Int, Int) -> [Maybe Int]
+getSectorNeighbors :: Board -> (Int, Int) -> [Square]
 getSectorNeighbors board@(Board rank arr) (i, j) =
   let i' = (i + rank - 1) `div` rank
       j' = (j + rank - 1) `div` rank
    in getSector board (i', j')
 
-getMissingNums :: Int -> [Maybe Int] -> Set.Set Int
-getMissingNums max maybes =
-  let nums = maybes & (filter isJust) & (map fromJust) & Set.fromList
+getMissingNums :: Int -> [Square] -> Set.Set Int
+getMissingNums max squares =
+  let nums = [x | Fixed x <- squares] & Set.fromList
    in [1 .. max] & filter (\x -> Set.notMember x nums) & Set.fromList
 
-hasDuplicates :: [Maybe Int] -> Bool
-hasDuplicates xs = (Set.size $ Set.fromList xs) == length xs
+hasDuplicates :: [Square] -> Bool
+hasDuplicates xs = (Set.size $ getNumbers xs) == length xs
 
 getPotentialValues :: (Int, Int) -> Board -> Set.Set Int
 getPotentialValues (i, j) board@(Board rank arr) =
@@ -68,7 +75,7 @@ solveSudokuImpl empties board@(Board rank arr) = case empties of
         (i, j) : xs ->
           let
             potentials = getPotentialValues (i,j) board
-            nextStates = [Board rank $ arr // [((i, j), Just p)] | p <- Set.toList potentials]
+            nextStates = [Board rank $ arr // [((i, j), Fixed p)] | p <- Set.toList potentials]
             solutions = map (solveSudokuImpl xs) nextStates
             sol = find isJust solutions
           in
@@ -80,13 +87,13 @@ solveSudoku board = solveSudokuImpl (getEmptySquares board) board
 
 getEmptySquares :: Board -> [(Int, Int)]
 getEmptySquares (Board rank arr) =
-  [(i, j) | i <- [1 .. rank ^ 2], j <- [1 .. rank ^ 2], isNothing (arr ! (i, j))]
+  [(i, j) | i <- [1 .. rank ^ 2], j <- [1 .. rank ^ 2], isEmpty (arr ! (i, j))]
 
 printBoard :: Board -> IO ()
 printBoard board@(Board rank arr) = forM_ [1 .. rank ^ 2] $ \i -> do
   forM_ [1 .. rank ^ 2] $ \j -> do
-    let tile = arr ! (i, j)
-    putStr $ if isJust tile then show (fromJust tile) else "*"
+    let square = arr ! (i, j)
+    putStr $ toString square
     putStr " "
     if j `mod` rank == 0 && j /= rank ^ 2 then putStr "|" else return ()
   putStr "\n"
@@ -104,15 +111,19 @@ getRank = do
   line <- getLine
   return $ read line
 
-readRow :: Int -> [((Int, Int), Maybe Int)] -> Int -> IO ([((Int, Int), Maybe Int)])
+readRow :: Int -> [((Int, Int), Square)] -> Int -> IO ([((Int, Int), Square)])
 readRow rank soFar i = do
   line <- getLine
   let filtered = filter (/= '|') line
       nums = words filtered
       maybes = map readMaybe nums
+      maybe2square = \x -> case x of
+        Nothing -> Possible $ Set.fromList [1..rank^2]
+        Just n -> Fixed n
+      squares = map maybe2square maybes
       indices = [(i, j) | j <- [1 .. rank ^ 2]]
   if i `mod` rank == 0 && i /= rank ^ 2 then getLine else return ""
-  return $ soFar ++ indices `zip` maybes
+  return $ soFar ++ (indices `zip` squares)
 
 readBoard :: IO (Board)
 readBoard = do
